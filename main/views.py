7e60163a2af4acd3af289b47e2b90d02b82b153a
PanConQueso0 <101284386+PanConQueso0@users.noreply.github.com>
2022-10-23 23:00:35 -0300

    ඞبينناඞبينناඞبيننا
    صفحة مع مناظر رهيبة من جميلة مع فكرة من أجل وفي وجهات النظر

--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from asyncio.windows_events import NULL
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseBadRequest
 # Create your views here.
+
+def inicio(request):
+    return render(request, 'productos.html')
+
+def stock(request, id, num):
+    producto = Producto.objects.get(id = id)
+    producto.stockProducto = producto.stockProducto + num
+    
+    producto.save()
+
+    messages.success(request, 'Perfil modificado correctamente!')
+    return redirect('productos')
+
+def productos(request):
+    producto = Producto.objects.all()
+    contexto = {"pro": producto}
+    return render(request, 'productos.html', contexto)
